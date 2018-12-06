aeb06627d80d925e4ea323320c5bbab5542ea810
SUdoWinchester <luizeduardo2607@gmail.com>
2018-12-05 23:54:44 -0200

    Implementado controller para remoção de tipo de atendimento

---
+++ app/Http/Controllers/AttendmentType/AttendmentTypeDestroy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\AttendmentType;
+
+use App\Http\Controllers\Controller;
+use App\Models\AttendmentType;
+use App\Http\Resources\AttendmentType as AttendmentTypeResource;
+
+class AttendmentTypeDestroy extends Controller
+{
+    /**
+     * @param AttendmentType $assisted
+     * @return AttendmentTypeResource
+     */
+    public function __invoke(AttendmentType $attendmentType)
+    {
+        $attendmentType->delete();
+
+        return new AttendmentTypeResource($attendmentType);
+    }
+}
