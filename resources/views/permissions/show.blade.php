f2abc5569d5b94d4aec85c35afd575da005e180a
EnViKeyy <pierri.nvk@outlook.com>
2018-12-05 22:17:40 -0200

    Implementado formulário de visualização de permissão específica

--- resources/views/permissions/show.blade.php
+++ resources/views/permissions/show.blade.php
@@ -1,12 +1,12 @@
 @extends('adminlte::page')
 
-@section('title', 'Visualizar Assistido')
+@section('title', 'Visualizar Permissão')
 
 @section('css')
 @endsection
 
 @section('content_header')
-    <h1>Assistido {{ $assisted->name }}</h1>
+    <h1>Permissão: {{ $permission->description }}</h1>
 @stop
 
 @section('content')
@@ -15,31 +15,11 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-            <div class="col-md-6">
-                <h2>Informações Pessoais</h2>
+            <div class="col-md-12">
+                <h2>Informações Da Permissão</h2>
 
-                <p> Nome: {{ $assisted->name }} </p>
-                <p> CPF: {{ $assisted->cpf }} </p>
-                <p> Email: {{ $assisted->email }} </p>
-                <p> Profissao: {{ $assisted->profession }} </p>
-                <p> Data de Nascimento: {{ date('d/m/Y', strtotime($assisted->birth_date)) }} </p>
-                <p> RG: {{ $assisted->rg }} </p>
-                <p> Emissor do RG: {{ $assisted->rg_issuer }} </p>
-                <p> Genero: {{ $assisted->gender }} </p>
-                <p> Estado Civil: {{ $assisted->marital_status }} </p>
-                <p> Observacoes: {{ $assisted->note }} </p>
-            </div>
-
-            <div class="col-md-6">
-                <h2>Endereco</h2>
-                
-                <p> CEP: {{ $assisted->postcode }} </p>
-                <p> Rua: {{ $assisted->street }} </p>
-                <p> Numero: {{ $assisted->number }} </p>
-                <p> Bairro: {{ $assisted->neighborhood }} </p>
-                <p> UF: {{ $assisted->uf }} </p>
-                <p> Cidade: {{ $assisted->city }} </p>
-                <p> Complemento: {{ $assisted->complement }} </p>
+                <p> Nome: {{ $permission->name }} </p>
+                <p> Descrição: {{ $permission->description }} </p>
             </div>
         </div>
     </div>
