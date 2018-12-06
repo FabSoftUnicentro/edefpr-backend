4b47c26b22ad3ea3745f99f480855dec1e33ac4e
EnViKeyy <pierri.nvk@outlook.com>
2018-12-05 22:08:48 -0200

    Implementado controoler para retorno do formulário de criação de permissão

--- app/Http/Controllers/Permission/PermissionCreate.php
+++ app/Http/Controllers/Permission/PermissionCreate.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace App\Http\Controllers\Assisted;
+namespace App\Http\Controllers\Permission;
 
-use App\Forms\Assisted\AssistedForm;
+use App\Forms\Permission\PermissionForm;
 use App\Http\Controllers\Controller;
 
-class AssistedCreate extends Controller
+class PermissionCreate extends Controller
 {
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function __invoke()
     {
-        $form = $this->formBuilder->create(AssistedForm::class, [
-            'url' => route('assisteds.store'),
+        $form = $this->formBuilder->create(PermissionForm::class, [
+            'url' => route('permissions.store'),
             'method' => 'POST'
         ]);
 
-        return view('assisteds.create', [
+        return view('permissions.create', [
             'form' => $form
         ]);
     }
