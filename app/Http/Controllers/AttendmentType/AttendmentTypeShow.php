10afab740c2481ecbdb31f1c35bd9bcbdb699b96
SUdoWinchester <luizeduardo2607@gmail.com>
2018-12-05 23:58:30 -0200

    Implementado controller para visualização de um tipo de atendimento

---
+++ app/Http/Controllers/AttendmentType/AttendmentTypeShow.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\AttendmentType;
+
+use App\Http\Controllers\Controller;
+use App\Models\AttendmentType;
+
+class AttendmentTypeShow extends Controller
+{
+    /**
+     * @param Assisted $attendmentType
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function __invoke(AttendmentType $attendmentType)
+    {
+        return view('attendmentTypes.show', [
+            'attendmentType' => $attendmentType
+        ]);
+    }
+}
