adceabdbad7d9504f95294d14de5568809cf258e
EnViKeyy <pierri.nvk@outlook.com>
2018-12-05 22:18:36 -0200

    Implementado rotas de acesso de permissões

--- routes/web.php
+++ routes/web.php
@@ -87,4 +87,26 @@ $this->get('/internal-server-error', function () {
     return view('error.500');
 })->name('500');
 
+// Permission Routes
+$this->group(['middleware' => ['auth'], 'namespace' => 'Permission', 'prefix' => 'permission'], function() {
+    $this->group(['middleware' => ['permission:register-permission']], function () {
+        $this->get('/create', 'PermissionCreate')->name('permissions.create');
+        $this->post('/', 'PermissionStore')->name('permissions.store');
+    });
+
+    $this->group(['middleware' => ['permission:update-permission']], function () {
+        $this->get('/{permission}/edit', 'PermissionEdit')->name('permissions.edit');
+        $this->put('/{permission}', 'PermissionUpdate')->name('permissions.update');
+    });
+
+    $this->group(['middleware' => ['permission:list-permission']], function() {
+        $this->get('/list', 'PermissionIndex')->name('permissions.index');
+        $this->get('/{permission}', 'PermissionShow')->name('permissions.show');
+    });
+
+    $this->group(['middleware' => ['permission:delete-permission']], function () {
+        $this->delete('/{permission}', 'PermissionDestroy')->name('permissions.destroy');
+    });
+});
+
 Auth::routes();
