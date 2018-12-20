af504d0a9b13070f64b3155087d93612c7434590
HigorG <higorgardin@hotmail.com>
2018-12-19 22:41:55 -0200

    Adicionado evento de clique e checkbox referente a role

--- resources/views/roles/assign.blade.php
+++ resources/views/roles/assign.blade.php
@@ -15,13 +15,16 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
-            <div class="col-md-6">
-                <select onChange="selectRole()" id="selectOpt">
+            <div class="col-md-12">
+                <select id="selectOpt">
                 <option></option>
                     @foreach($roles as $role)
                         <option value="{{ $role->id }}"> {{ $role->description }} </option>
                     @endforeach
                 </select>
+            </div>
+            <div class="col-md-12 permissions" id="permissions">
+
             </div>
         </div>
     </div>
@@ -30,9 +33,31 @@
 @section('js')
     <script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script>
-        function selectRole() {
-            var selectedRole = document.getElementById("selectOpt");
-            alert(selectedRole.options[selectedRole.selectedIndex].value);
+        $('#selectOpt').on('change', function () {
+            var selectedRole = $('#selectOpt');
+            var permissionDiv = $('#permissions');
+            var roleId = selectedRole.val();
+
+            $.ajax ({
+                method: 'GET',
+                url: '{{ route('roles.permissions', '_role') }}'.replace('_role', roleId),
+                success: function(data) {
+                    $.each(data, function(i, val) {
+                        if (val.chosen) {
+                            permissionDiv.append('<input type="checkbox" checked onclick="fogoNaBabilonia()">' + val.name + '</br>');
+                        } else {
+                            permissionDiv.append('<input type="checkbox" onclick="fogoNaBabilonia()"/>' + val.name + '</br>');
+                        }
+                    });
+                },
+                error: function(data) {
+
+                }
+            });
+        });
+
+        function fogoNaBabilonia () {
+            alert("DJ CLEITON RASTA AO SOM DO CABEÇA DE GELO OLHA PEDRA")
         }
     </script>
 @endsection
