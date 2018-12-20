398f1bcb429d7e9c324a181ac3e6001208238ba4
HigorG <higorgardin@hotmail.com>
2018-12-19 22:42:39 -0200

    Criado controller para listar permissoes sem paginacao

---
+++ app/Http/Controllers/Permission/PermissionWithoutPaginate.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Permission;
+
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use App\Http\Resources\Permissions as ResourcePermission;
+use Illuminate\Http\Request;
+
+class PermissionWithoutPaginate extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function __invoke(Request $request)
+    {
+        $permissions = Permission::All();
+
+        return $permissions;
+    }
+}
