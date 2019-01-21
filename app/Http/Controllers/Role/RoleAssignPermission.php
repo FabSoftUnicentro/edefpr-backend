2df583cc8b849a6b674819e043798e5ddebb6a79
EnViKeyy <pierri.nvk@outlook.com>
2019-01-21 18:23:20 -0200

    Implementado controller para atribuição de permissão para role

--- app/Http/Controllers/Role/RoleAssignPermission.php
+++ app/Http/Controllers/Role/RoleAssignPermission.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers\Role;
 
 use App\Http\Controllers\Controller;
+use App\Models\Permission;
 use App\Models\Role;
-use Illuminate\Http\Request;
+use App\Http\Resources\Role as RoleResource;
 
 class RoleAssignPermission extends Controller
 {
     /**
-     * @param Request $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @param Role $role
+     * @param Permission $permission
+     * @return RoleResource
      */
-    public function __invoke(Request $request)
+    public function __invoke(Role $role, Permission $permission)
     {
-        $roles = Role::all();
-        
-        return view('roles.assign', [
-            'roles' => $roles,
-        ]);
+        $role->givePermissionTo($permission);
+
+        return new RoleResource($role);
     }
 }
