b861db7c9e4a89a26d6fff5a39f50f522d6ccb7e
EnViKeyy <pierri.nvk@outlook.com>
2019-01-29 10:43:56 -0200

    Implementado controller para visualização da view de permissões do usuário

--- app/Http/Controllers/User/UserPermission.php
+++ app/Http/Controllers/User/UserPermission.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 1/29/2019
- * Time: 10:10 AM
- */
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserPermission extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function __invoke(Request $request)
+    {
+        $users = User::all();
+
+        return view('users.assign', [
+            'users' => $users,
+        ]);
+    }
+}
