69a66cf07e3c5cb60fa034f48c658cd66376efdd
EnViKeyy <pierri.nvk@outlook.com>
2019-02-09 17:55:44 -0200

    Implementado view para visualização de permissões do usuário e de role

--- resources/views/users/permission.blade.php
+++ resources/views/users/permission.blade.php
@@ -1,83 +1,46 @@
-@extends('adminlte::page')
-
-@section('title', 'Visualizar nível de Acesso')
-
-@section('css')
-@endsection
-
-@section('content_header')
-    <h1>Nível de Acesso</h1>
-@stop
-
-@section('content')
-    <div class="box">
-        <div class="box-header with-border">
+<div class="row">
+    <p>Permissões específicas do cargo</p>
+    @foreach($rolePermissions as $rolePermission)
+        <div class="col-md-4">
+            <input type="checkbox" checked disabled />
+            {{ $rolePermission['description'] }}
         </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-            <div class="col-md-12">
-                <p>Selecione o funcionário para atribuição/desatribuição de permissão<p/>
-                <select id="selectOpt">
-                    <option></option>
-                    @foreach($users as $user)
-                        <option value="{{ $user->id }}"> {{ $user->name }} </option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-md-12 permissions" id="permissions">
+    @endforeach
+    <br/>
+</div>
 
-            </div>
-        </div>
-    </div>
-@stop
+<div class="row">
+    <br/>
+    <p>Permissões específicas do funcionário</p>
+    @foreach($permissions as $permission)
+            @if ($permission['chosen'])
+                <div class="col-md-4">
+                    <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" checked />
+                    {{ $permission['description'] }}
+                </div>
+            @else
+                <div class="col-md-4">
+                    <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" />
+                    {{ $permission['description'] }}
+                </div>
+            @endif
+    @endforeach
+</div>
 
-@section('js')
-    <script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <script>
-        $('#selectOpt').click(function() {
-            var selectedUser = $('#selectOpt');
-            var permissionDiv = $('#permissions');
-            var UserId = selectedUser.val();
-            $("#chkPassport").click(function () {
-                if ($(this).is(":checked")) {
-                    $("#dvPassport").show();
-                } else {
-                    $("#dvPassport").hide();
-                }
+<script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script>
+    function check_uncheck_checkbox(permissionId) {
+        if ($(permissionId).checked) {
+            $.ajax({
+                method: 'PUT',
+                url: '{{ route('users.unassign.permission', ['_user', '_permissionId']) }}'.replace('_user',  {{ $user->id }}).replace('_permissionId', permissionId),
             });
-            $.ajax ({
-                method: 'GET',
-                url: '{{ route('users.permissions', '_user') }}'.replace('_user', UserId),
-                success: function(data) {
-                    $.each(data, function(i, val) {
-                        if (val.chosen) {
-                            permissionDiv.append('<input id="'+val.id+'" type="checkbox" ' +
-                                'onclick="check_uncheck_checkbox(id)" checked>'+val.description+'<br/>');
-                        } else {
-                            permissionDiv.append('<input id="'+val.id+'" type="checkbox" ' +
-                                'onclick="check_uncheck_checkbox(id)"/>'+val.description+'<br/>');
-                        }
-                    });
-                },
-                error: function(data) {
-                }
-            });
-        });
-        function check_uncheck_checkbox(permissionId) {
-            var selectedUser = $('#selectOpt');
-            var userId = selectedUser.val();
-            if (document.getElementById(permissionId).checked) {
-                $.ajax({
-                    method: 'PUT',
-                    url: '{{ route('users.assign.permission', ['_user', '_permissionId']) }}'.replace('_user',  userId).replace('_permissionId', permissionId),
-                });
-            }
-            else {
-                $.ajax({
-                    method: 'PUT',
-                    url: '{{ route('users.unassign.permission', ['_user', '_permissionId']) }}'.replace('_user',  userId).replace('_permissionId', permissionId),
-                });
-            }
         }
-    </script>
-@endsection
+        else {
+            $.ajax({
+                method: 'PUT',
+                url: '{{ route('users.assign.permission', ['_user', '_permissionId']) }}'.replace('_user',  {{ $user->id }}).replace('_permissionId', permissionId),
+            });
+        }
+    }
+</script>
