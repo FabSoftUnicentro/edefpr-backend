bc73784f1f2dca9534bccef128c6eae11bbd16ec
EnViKeyy <pierri.nvk@outlook.com>
2019-02-12 23:23:47 -0200

    Corrigido bug que verificava o status da checkbox

--- resources/views/users/permission.blade.php
+++ resources/views/users/permission.blade.php
@@ -13,24 +13,24 @@
     <br/>
     <p>Permissões específicas do funcionário</p>
     @foreach($permissions as $permission)
-            @if ($permission['chosen'])
-                <div class="col-md-4">
-                    <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" checked />
-                    {{ $permission['description'] }}
-                </div>
-            @else
-                <div class="col-md-4">
-                    <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" />
-                    {{ $permission['description'] }}
-                </div>
-            @endif
+        @if ($permission['chosen'])
+            <div class="col-md-4">
+                <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" checked />
+                {{ $permission['description'] }}
+            </div>
+        @else
+            <div class="col-md-4">
+                <input id="permission-{{ $permission['id'] }}" type="checkbox" onclick="check_uncheck_checkbox({{ $permission['id'] }})" />
+                {{ $permission['description'] }}
+            </div>
+        @endif
     @endforeach
 </div>
 
 <script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>
     function check_uncheck_checkbox(permissionId) {
-        if ($(permissionId).checked) {
+        if (!$('input[type=checkbox]').prop('checked')) {
             $.ajax({
                 method: 'PUT',
                 url: '{{ route('users.unassign.permission', ['_user', '_permissionId']) }}'.replace('_user',  {{ $user->id }}).replace('_permissionId', permissionId),
