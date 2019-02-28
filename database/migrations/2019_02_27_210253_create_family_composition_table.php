186c9e61d30703d2c8b0d93e281ee1640fdff2f2
HigorG <higorgardin@hotmail.com>
2019-02-27 22:05:11 -0300

    Criado migration de FamilyComposition

---
+++ database/migrations/2019_02_27_210253_create_family_composition_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamilyCompositionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('family_compositions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('situation');
+            $table->enum('kinship', ['daughter','son','cousin','sister','brother','mother','father','grandmother','grandfather','uncle','aunt','wife','husband','grandson','granddaughter','nephew','niece','stepfather','stepmother']);
+            $table->string('work');
+            $table->double('income');
+            $table->integer('assisted_id')->unsigned();
+            $table->foreign('assisted_id')->references('id')->on('assisteds');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('family_compositions');
+    }
+}
