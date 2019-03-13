2eafd431e73e28d1438437cf1e3778e976f1ebfe
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-13 20:13:29 -0300

    Implementado rotas do counter part

--- routes/web/counter_part.php
+++ routes/web/counter_part.php
@@ -1,23 +1,21 @@
 <?php
 
 // CounterPart routes
-$this->group(['middleware' => ['auth'], 'namespace' => 'CounterPart', 'prefix' => 'counter-part'],
-    function() {
-        $this->group(['middleware' => ['permission:register-attendment']], function () {
-            $this->get('/create', 'CounterPartCreate')->name('counterParts.create');
-            $this->post('/', 'CounterPartStore')->name('counterParts.store');
-        });
+$this->group(['middleware' => ['permission:register-counterPart']], function () {
+    $this->get('/create', 'CounterPartCreate')->name('counterParts.create');
+    $this->post('/', 'CounterPartStore')->name('counterParts.store');
+});
 
-        $this->group(['middleware' => ['permission:update-attendment']], function () {
-            $this->get('/{counterPart}/edit', 'CounterPartEdit')->name('counterParts.edit');
-            $this->put('/{counterPart}', 'CounterPartUpdate')->name('counterParts.update');
-        });
+$this->group(['middleware' => ['permission:update-counterPart']], function () {
+    $this->get('/{counterPart}/edit', 'CounterPartEdit')->name('counterParts.edit');
+    $this->put('/{counterPart}', 'CounterPartUpdate')->name('counterParts.update');
+});
 
-        $this->group(['middleware' => ['permission:list-attendment']], function () {
-            $this->get('/list', 'CounterPartIndex')->name('counterParts.index');
-            $this->get('/{counterPart}', 'CounterPartShow')->name('counterParts.show');
-        });
-        $this->group(['middleware' => ['permission:delete-attendment']], function () {
-            $this->delete('/{counterPart}', 'CounterPartDestroy')->name('counterParts.destroy');
-        });
-    });
+$this->group(['middleware' => ['permission:register-counterPart']], function () {
+    $this->get('/list', 'CounterPartIndex')->name('counterParts.index');
+    $this->get('/{counterPart}', 'CounterPartShow')->name('counterParts.show');
+});
+
+$this->group(['middleware' => ['permission:delete-counterPart']], function () {
+    $this->delete('/{counterPart}', 'CounterPartDestroy')->name('counterParts.destroy');
+});
