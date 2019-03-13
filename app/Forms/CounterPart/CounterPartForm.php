4f0f5fc802d23825d36ff83f5c096d1f0297c13d
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-13 15:06:15 -0300

    Implementado formulario do counterPart

---
+++ app/Forms/CounterPart/CounterPartForm.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Forms\CounterPart;
+
+use App\Forms\Field;
+use Kris\LaravelFormBuilder\Form;
+
+class CounterPartForm extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('name', Field::TEXT, [
+                'label' => 'Nome',
+                'rules' => 'required|string'
+            ])
+            ->add('rg', Field::TEXT, [
+                'label' => 'RG',
+                'rules' => 'numeric'
+            ])
+            ->add('rg_issuer', Field::TEXT, [
+                'label' => 'Órgão Emissor'
+            ])
+            ->add('phone_number', Field::TEXT, [
+                'label' => 'Número de telefone',
+                'rules' => 'required'
+            ])
+            ->add('remuneration', Field::TEXT, [
+                'label' => 'Remuneração',
+                'rules' => 'required|numeric'
+            ])
+            ->add('profession', Field::TEXT, [
+                'label' => 'Profissão',
+                'rules' => 'string'
+            ])
+            ->add('note', Field::TEXTAREA, [
+                'label' => 'Dados do trabalho',
+                'rules' => 'string'
+            ])
+            ->add('cpf', Field::TEXT, [
+                'label' => 'CPF',
+                'rules' => 'required|numeric'
+            ])
+            ->add('postcode', Field::TEXT, [
+                'label' => 'CEP',
+                'rules' => 'required|string'
+            ])
+            ->add('street', Field::TEXT, [
+                'label' => 'Rua',
+                'rules' => 'required|string'
+            ])
+            ->add('neighborhood', Field::TEXT, [
+                'label' => 'Bairro',
+                'rules' => 'required|string'
+            ])
+            ->add('number', Field::TEXT, [
+                'label' => 'Número',
+                'rules' => 'required|string'
+            ])
+            ->add('complement', Field::TEXT, [
+                'label' => 'Complemento',
+                'rules' => 'string'
+            ])
+            ->add('uf', Field::TEXT, [
+                'label' => 'UF',
+                'rules' => 'required|string'
+            ])
+            ->add('city', Field::TEXT, [
+                'label' => 'Cidade',
+                'rules' => 'required|string'
+            ])
+            ->add('submit', Field::BUTTON_SUBMIT, [
+                'label' => 'Salvar'
+            ]);
+    }
+}
