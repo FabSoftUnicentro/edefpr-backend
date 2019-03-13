0572e6c2d35e45b49a7497dc821b4db8bfb1cca9
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-13 14:57:23 -0300

    Refatorado factory de CounterPart

--- database/factories/CounterPartFactory.php
+++ database/factories/CounterPartFactory.php
@@ -17,16 +17,13 @@ use App\Models\CounterPart;
 $factory->define(CounterPart::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'birth_date' => $faker->date(),
         'rg' => $faker->unique()->text(11),
-        'rg_issuer' => 'SSP',
-        'gender' => 'M',
+        'rg_issuer' => 'SSP', // Pode ser removido no futuro
         'profession' => 'Teste',
         'note' => null,
-        'document_type' => 'cpf',
-        'document_number' => $faker->numberBetween(999999999),
-        'fantasy_name' => null,
+        'remuneration' => 0.00,
+        'phone_number' => $faker->numberBetween(99999999999),
+        'cpf' => $faker->numberBetween(999999999),
         'uf' => 'PR',
         'city' => 1,
         'number' => '123',
--- routes/web.php
+++ routes/web.php
@@ -183,6 +183,28 @@ $this->group(['middleware' => ['auth'], 'namespace' => 'Attendment', 'prefix' =>
         });
 });
 
+// CounterPart routes
+$this->group(['middleware' => ['auth'], 'namespace' => 'CounterPart', 'prefix' => 'counter-part'],
+    function() {
+        $this->group(['middleware' => ['permission:register-attendment']], function () {
+            $this->get('/create', 'CounterPartCreate')->name('counterParts.create');
+            $this->post('/', 'CounterPartStore')->name('counterParts.store');
+        });
+
+        $this->group(['middleware' => ['permission:update-attendment']], function () {
+            $this->get('/{counterPart}/edit', 'CounterPartEdit')->name('counterParts.edit');
+            $this->put('/{counterPart}', 'CounterPartUpdate')->name('counterParts.update');
+        });
+
+        $this->group(['middleware' => ['permission:list-attendment']], function () {
+            $this->get('/list', 'CounterPartIndex')->name('counterParts.index');
+            $this->get('/{counterPart}', 'CounterPartShow')->name('counterParts.show');
+        });
+        $this->group(['middleware' => ['permission:delete-attendment']], function () {
+            $this->delete('/{counterPart}', 'CounterPartDestroy')->name('counterParts.destroy');
+        });
+    });
+
 // Postcode routes
 $this->group(['middleware' => ['auth'], 'namespace' => 'Postcode', 'prefix' => 'postcode'], function() {
     $this->get('/{postcode}', 'PostcodeSearch')->name('postcode.search');
