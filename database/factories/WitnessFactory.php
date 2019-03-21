536728f5568fdf84ce767a80a77f35ad1c8dbd07
EnViKeyy <pierri.nvk@outlook.com>
2019-03-20 21:09:11 -0300

    Implementado factory de testemunhas

--- database/factories/WitnessFactory.php
+++ database/factories/WitnessFactory.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Models\City;
-use App\Models\User;
+use App\Models\Witness;
 use Faker\Generator as Faker;
 
 /*
@@ -15,27 +14,18 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Witness::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'cpf' => (string) $faker->numberBetween(999999999),
-        'birth_date' => $faker->date(),
-        'birthplace' => DB::table('cities')->exists() ? DB::table('cities')->inRandomOrder()->first()->id : factory(City::class)->create(),
         'rg' => $faker->unique()->text(11),
         'rg_issuer' => 'SSP',
-        'gender' => 'M',
-        'marital_status' => 'Solteiro',
-        'note' => null,
         'uf' => 'PR',
-        'city' => 1,
+        'city' => 'Guarapuava',
         'number' => '123',
         'street' => 'Teste',
         'postcode' => '85015310',
         'complement' => '',
         'neighborhood' => 'Batel',
-        'remember_token' => str_random(10),
-        'must_change_password' => (string) 1
     ];
 });
