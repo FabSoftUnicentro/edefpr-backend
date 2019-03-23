eb90905b714fbb3e60c93d4a84880d8a330d340c
EnViKeyy <pierri.nvk@outlook.com>
2019-03-22 21:29:25 -0300

    Implementado arquivo de traduções

--- resources/lang/pt-br/translations.php
+++ resources/lang/pt-br/translations.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jean Pierri
- * Date: 3/22/2019
- * Time: 8:12 PM
- */
+
+return [
+    'kinship' => [
+        'mother' => 'Mãe',
+        'father' => 'Pai',
+        'son' => 'Filho',
+        'daughter' => 'Filha',
+        'sister' => 'Irmã',
+        'brother' => 'Irmão',
+        'grandmother' => 'Avó',
+        'grandfather' => 'Avô',
+        'grandson' => 'Neto',
+        'granddaughter' => 'Neta',
+        'uncle' => 'Tio',
+        'aunt' => 'Tia',
+        'nephew' => 'Sobrinho',
+        'niece' => 'Sobrinha',
+        'cousin' => 'Primo/Prima',
+        'wife' => 'Esposa',
+        'husband' => 'Esposo',
+        'stepfather' => 'Padastro',
+        'stepmother' => 'Madastra',
+        'stepbrother' => 'Meio-irmão',
+        'stepsister' => 'Meio-Irmã',
+    ],
+    'legal_situation' => [
+        'general' => 'Geral',
+        'elderly' => 'Idoso',
+        'child' => 'Criança/Adolescente',
+        'disabled-person' => 'Pessoa com deficiência',
+        'ex-prisoner' => 'Egresso Sistema Prisional'
+    ],
+    'gender' => [
+        'M' => 'Masculino',
+        'F' => 'Feminino',
+        'O' => 'Outro',
+        'I' => 'Indefinido'
+    ]
+];
