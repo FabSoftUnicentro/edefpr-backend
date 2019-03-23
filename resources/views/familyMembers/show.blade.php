b2895d6d2dc7340a1719c2ccc4efa743f78467a3
EnViKeyy <pierri.nvk@outlook.com>
2019-03-23 03:13:06 -0300

    Refatoração de código

--- resources/views/familyMembers/show.blade.php
+++ resources/views/familyMembers/show.blade.php
@@ -18,13 +18,13 @@
             <div class="box-body">
                 <h3>Informações Pessoais</h3>
 
-                    <p> <b>Nome:</b> {{ $familyMember->name }} </p>
-                    <p> <b>Assitido:</b> {{ $familyMember->assisted->name }} </p>
-                    <p> <b>Situação Legal:</b> {{ __('translations.legal_situation.'.$familyMember->legal_situation) }} </p>
-                    <p> <b>Grau de Parentesco:</b> {{ __('translations.kinship.'.$familyMember->kinship) }}</p>
-                    <p> <b>Data de Nascimento:</b> {{ date('d/m/Y', strtotime($familyMember->birth_date)) }} </p>
-                    <p> <b>Tabalho:</b> {{ $familyMember->work }} </p>
-                    <p> <b>Renda:</b> R$ {{ $familyMember->income }} </p>
+                <p> <b>Nome:</b> {{ $familyMember->name }} </p>
+                <p> <b>Assitido:</b> {{ $familyMember->assisted->name }} </p>
+                <p> <b>Situação Legal:</b> {{ __('translations.legal_situation.'.$familyMember->legal_situation) }} </p>
+                <p> <b>Grau de Parentesco:</b> {{ __('translations.kinship.'.$familyMember->kinship) }}</p>
+                <p> <b>Data de Nascimento:</b> {{ date('d/m/Y', strtotime($familyMember->birth_date)) }} </p>
+                <p> <b>Tabalho:</b> {{ $familyMember->work }} </p>
+                <p> <b>Renda:</b> R$ {{ $familyMember->income }} </p>
             </div>
         </div>
     </div>
