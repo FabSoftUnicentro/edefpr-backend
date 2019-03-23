dac4ece5d9c6330dbc2d9828c7e45bd6b07d69c6
EnViKeyy <pierri.nvk@outlook.com>
2019-03-23 13:42:47 -0300

    Corregido nomes de atributos

--- resources/views/familyMembers/show.blade.php
+++ resources/views/familyMembers/show.blade.php
@@ -19,7 +19,7 @@
                 <h3>Informações Pessoais</h3>
 
                 <p> <b>Nome:</b> {{ $familyMember->name }} </p>
-                <p> <b>Assitido:</b> {{ $familyMember->assisted->name }} </p>
+                <p> <b>Assistido:</b> {{ $familyMember->assisted->name }} </p>
                 <p> <b>Situação Legal:</b> {{ __('translations.legal_situation.'.$familyMember->legal_situation) }} </p>
                 <p> <b>Grau de Parentesco:</b> {{ __('translations.kinship.'.$familyMember->kinship) }}</p>
                 <p> <b>Data de Nascimento:</b> {{ date('d/m/Y', strtotime($familyMember->birth_date)) }} </p>
