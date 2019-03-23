81bf974a7493441754874a931de7286ff492b4fe
EnViKeyy <pierri.nvk@outlook.com>
2019-03-22 21:24:47 -0300

    Implementado form de composição familiar

--- app/Forms/FamilyComposition/FamilyComposition.php
+++ app/Forms/FamilyComposition/FamilyComposition.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Forms\Assisted;
+namespace App\Forms\FamilyComposition;
 
 use App\Forms\Field;
+use App\Models\Assisted;
 use Kris\LaravelFormBuilder\Form;
 
-class AssistedForm extends Form
+class FamilyCompositionForm extends Form
 {
     public function buildForm()
     {
@@ -14,76 +15,65 @@ class AssistedForm extends Form
                 'label' => 'Nome',
                 'rules' => 'required|string'
             ])
-            ->add('email', Field::EMAIL, [
-                'label' => 'Email',
-                'rules' => 'required|email|unique:assisteds'
-            ])
-            ->add('cpf', Field::TEXT, [
-                'label' => 'CPF',
-                'rules' => 'required|unique:users'
-            ])
-            ->add('profession', Field::TEXT, [
-                'label' => 'Profissão',
+            ->add('work', Field::TEXT, [
+                'label' => 'Trabalho',
                 'rules' => 'required|string'
             ])
             ->add('birth_date', Field::DATE, [
                 'label' => 'Data de Nascimento',
                 'rules' => 'required'
             ])
-            ->add('rg', Field::TEXT, [
-                'label' => 'Documento de Identidade',
-                'rules' => 'required|string'
+            ->add('income', Field::TEXT, [
+                'label' => 'Renda',
+                'rules' => 'required|double'
             ])
-            ->add('rg_issuer', Field::TEXT, [
-                'label' => 'Emissor do Documento de Identidade',
-                'rules' => 'required|string'
-            ])
-            ->add('gender', Field::SELECT, [
-                'label' => 'Gênero',
+            ->add('legal_situation', Field::SELECT, [
+                'label' => 'Situação Legal',
                 'choices' => [
-                    'M' => 'Masculino',
-                    'F' => 'Feminino',
-                    'O' => 'Outro',
-                    'I' => 'Indefinido'
+                    'general' => 'Geral',
+                    'elderly' => 'Pessoa Idosa',
+                    'child' => 'Criança/Adolescente',
+                    'disabled-person' => 'Pessoa com deficiência',
+                    'ex-prisoner' => 'Egresso Sistema Prisional'
                 ],
-                'empty_value' => 'Selecione um gênero',
+                'empty_value' => 'Selecione a situação legal da pessoa',
                 'rules' => 'required'
             ])
-            ->add('marital_status', Field::TEXT, [
-                'label' => 'Estado Civil',
-                'rules' => 'required|string'
+            ->add('kinship', Field::SELECT, [
+                'label' => 'Grau de Parentesco',
+                'choices' => [
+                    'mother' => 'Mãe',
+                    'father' => 'Pai',
+                    'son' => 'Filho',
+                    'daughter' => 'Filha',
+                    'sister' => 'Irmã',
+                    'brother' => 'Irmão',
+                    'grandmother' => 'Avó',
+                    'grandfather' => 'Avô',
+                    'grandson' => 'Neto',
+                    'granddaughter' => 'Neta',
+                    'uncle' => 'Tio',
+                    'aunt' => 'Tia',
+                    'nephew' => 'Sobrinho',
+                    'niece' => 'Sobrinha',
+                    'cousin' => 'Primo/Prima',
+                    'wife' => 'Esposa',
+                    'husband' => 'Esposo',
+                    'stepfather' => 'Padastro',
+                    'stepmother' => 'Madastra',
+                    'stepbrother' => 'Meio-irmão',
+                    'stepsister' => 'Meio-Irmã'
+                ],
+                'empty_value' => 'Selecione um grau de parentesco',
+                'rules' => 'required'
             ])
-            ->add('note', Field::TEXTAREA, [
-                'label' => 'Observações',
-                'rules' => 'string'
-            ])
-            ->add('postcode', Field::TEXT, [
-                'label' => 'CEP',
-                'rules' => 'required|string'
-            ])
-            ->add('street', Field::TEXT, [
-                'label' => 'Rua',
-                'rules' => 'required|string'
-            ])
-            ->add('number', Field::TEXT, [
-                'label' => 'Número',
-                'rules' => 'required|string'
-            ])
-            ->add('neighborhood', Field::TEXT, [
-                'label' => 'Bairro',
-                'rules' => 'required|string'
-            ])
-            ->add('complement', Field::TEXT, [
-                'label' => 'Complemento',
-                'rules' => 'string'
-            ])
-            ->add('city', Field::TEXT, [
-                'label' => 'Cidade',
-                'rules' => 'required|string'
-            ])
-            ->add('uf', Field::TEXT, [
-                'label' => 'UF',
-                'rules' => 'required|string'
+            ->add('assisted_id', 'entity', [
+                'label' => 'Assistido',
+                'class' => Assisted::class,
+                'property' => 'name',
+                'rules' => 'required',
+                'empty_value' => 'Selecione um assistido',
+                'empty_data' => null
             ])
             ->add('submit', Field::BUTTON_SUBMIT, [
                 'label' => 'Salvar'
