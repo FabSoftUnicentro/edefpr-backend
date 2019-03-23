bfde6c3be87d9fce42d48de5f74ec4eba579a3b9
EnViKeyy <pierri.nvk@outlook.com>
2019-03-22 21:25:12 -0300

    Implementado resource de composição familiar

--- app/Http/Resources/FamilyComposition.php
+++ app/Http/Resources/FamilyComposition.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Assisted extends JsonResource
+class FamilyComposition extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,18 +17,12 @@ class Assisted extends JsonResource
         // return parent::toArray($request);
         return [
             'name' => $this->name,
-            'email' => $this->email,
-            'cpf' => $this->cpf,
             'birth_date' => $this->birth_date,
-            'birthplace' => $this->birthplace,
-            'rg' => $this->rg,
-            'rg_issuer' => $this->rg_issuer,
-            'gender' => $this->gender,
-            'marital_status' => $this->marital_status,
-            'profession' => $this->profession,
-            //'counter_part' => $this->counter_part,
-            'note' => $this->note,
-            'addresses' => json_decode($this->addresses, true)
+            'work' => $this->work,
+            'income' => $this->income,
+            'legal_situation' => $this->legal_situation,
+            'kinship' => $this->kinship,
+            'assisted_id' => $this->assisted_id
         ];
     }
 
