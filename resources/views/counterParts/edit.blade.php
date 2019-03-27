738880c366911f93d932000c00f47ee1762e61ec
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-27 14:13:55 -0300

    Implementado mascara de dinheiro, CPF, CNPJ e lista de estados/cidade

--- resources/views/counterParts/edit.blade.php
+++ resources/views/counterParts/edit.blade.php
@@ -15,3 +15,43 @@
         'form' => $form
     ])
 @stop
+
+@section('js')
+    <script src="{{ asset('js/jquery.mask.min.js') }}"></script>
+    <script>
+        $("#remuneration").mask('000.000.000,00', { reverse: true });
+        $("#document_number").mask('000.000.000-00');
+    </script>
+
+    <script>
+        const masks = ['000.000.000-00', '00.000.000/0000-00'];
+
+        $("#document_type").change(function (e) {
+            if ($("#document_type")[0].value ==  "CPF") {
+                $("#document_number").mask(masks[0]);
+            } else {
+                $("#document_number").mask(masks[1]);
+            }
+        })
+    </script>
+
+    <script>
+        $("#uf").change(function (e) {
+            const uf = $("#uf")[0].value;
+
+            $.ajax({
+                url: '{{ route('utils.get_cities', '_uf') }}'.replace('_uf', uf),
+                method: 'POST',
+                success: function (xhr) {
+                    $("#city").empty();
+                    $.each(xhr.cities, function(key, value) {
+                        $("#city").append($('<option></option>').attr('value', value).text(key));
+                    });
+                },
+                error: function (xhr) {
+
+                }
+            })
+        })
+    </script>
+@endsection
