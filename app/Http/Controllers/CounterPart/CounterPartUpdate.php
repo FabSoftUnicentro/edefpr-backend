6a6e5a7579613c0791004c44f6d61301a5ecebdb
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-27 14:11:39 -0300

    Adicionado validação de CPF/CNPJ

--- app/Http/Controllers/CounterPart/CounterPartUpdate.php
+++ app/Http/Controllers/CounterPart/CounterPartUpdate.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers\CounterPart;
 
 use App\Http\Controllers\Controller;
 use App\Models\CounterPart;
+use App\Utils\Document;
 use Illuminate\Http\Request;
 
 class CounterPartUpdate extends Controller
@@ -21,7 +22,33 @@ class CounterPartUpdate extends Controller
      */
     public function __invoke(Request $request, CounterPart $counterPart)
     {
-        $counterPart->update($request->all());
+        if ($request['document_type'] === (Document::TYPE_CPF)) {
+            if (!Document::isCpf($request['document_number'])) {
+                return redirect()
+                    ->back()
+                    ->with('alert-danger', 'Falha ao cadastrar, CPF incorreto!');
+            }
+        } elseif ($request['document_type'] === (Document::TYPE_CNPJ)) {
+            if (!Document::isCnpj($request['document_number'])) {
+                return redirect()
+                    ->back()
+                    ->with('alert-danger', 'Falha ao cadastrar, CNPJ incorreto!');
+            }
+        } else {
+            return redirect()
+                ->back()
+                ->with('alert-danger', 'Falha ao cadastrar, verifique o tipo do documento indentificador!');
+        }
+
+        $params = $request->all();
+
+        $remuneration = $params['remuneration'];
+        $remuneration = str_replace('.', '', $remuneration);
+        $remuneration = str_replace(',', '.', $remuneration);
+
+        $params['remuneration'] = floatval($remuneration);
+
+        $counterPart->update($params);
 
         try {
             $counterPart->save();
