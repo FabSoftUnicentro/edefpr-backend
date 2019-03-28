875394db24ed2c9778a523316d1f51c178795cc4
SUdoWinchester <luizeduardo2607@gmail.com>
2019-03-27 21:51:07 -0300

    Removido if

--- app/Http/Controllers/CounterPart/CounterPartUpdate.php
+++ app/Http/Controllers/CounterPart/CounterPartUpdate.php
@@ -22,33 +22,7 @@ class CounterPartUpdate extends Controller
      */
     public function __invoke(Request $request, CounterPart $counterPart)
     {
-        if ($request['document_type'] === (Document::TYPE_CPF)) {
-            if (!Document::isCpf($request['document_number'])) {
-                return redirect()
-                    ->back()
-                    ->with('alert-danger', 'Falha ao cadastrar, CPF incorreto!');
-            }
-        } elseif ($request['document_type'] === (Document::TYPE_CNPJ)) {
-            if (!Document::isCnpj($request['document_number'])) {
-                return redirect()
-                    ->back()
-                    ->with('alert-danger', 'Falha ao cadastrar, CNPJ incorreto!');
-            }
-        } else {
-            return redirect()
-                ->back()
-                ->with('alert-danger', 'Falha ao cadastrar, verifique o tipo do documento indentificador!');
-        }
-
-        $params = $request->all();
-
-        $remuneration = $params['remuneration'];
-        $remuneration = str_replace('.', '', $remuneration);
-        $remuneration = str_replace(',', '.', $remuneration);
-
-        $params['remuneration'] = floatval($remuneration);
-
-        $counterPart->update($params);
+        $counterPart->update($request->all());
 
         try {
             $counterPart->save();
