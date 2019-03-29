6bcd2c573997bf262aaa87ff12611ba1e314d908
EnViKeyy <pierri.nvk@outlook.com>
2019-03-28 21:13:42 -0300

    Implementado model de processo

---
+++ app/Models/Process.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Process extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'description',
+        'assisted_id',
+        'counter_part_id'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * Get the assisted one that has the familiar member.
+     */
+    public function assisted()
+    {
+        return $this->belongsTo(Assisted::class);
+    }
+
+    /**
+     * Get the assisted one that has the familiar member.
+     */
+    public function counterPart()
+    {
+        return $this->belongsTo(CounterPart::class);
+    }
+}
