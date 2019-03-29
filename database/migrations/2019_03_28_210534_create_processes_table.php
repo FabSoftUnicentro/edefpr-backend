10153874673bb5b96851186b5aa6df982b186476
EnViKeyy <pierri.nvk@outlook.com>
2019-03-28 21:13:26 -0300

    Implementado migrations de processo

---
+++ database/migrations/2019_03_28_210534_create_processes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProcessesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('processes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->integer('assisted_id')->unsigned();
+            $table->foreign('assisted_id')->references('id')->on('assisteds');
+            $table->integer('counter_part_id')->unsigned();
+            $table->foreign('counter_part_id')->references('id')->on('counterpart');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('processes');
+    }
+}
