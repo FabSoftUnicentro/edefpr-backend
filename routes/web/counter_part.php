11e3d726f904cf1572a0c7a692f8152148a4bcd3
SUdoWinchester <luizeduardo2607@gmail.com>
2019-04-02 11:15:42 -0300

    Atualizado rotas

--- routes/web/counter_part.php
+++ routes/web/counter_part.php
@@ -1,21 +1,21 @@
 <?php
 
 // CounterPart routes
-$this->group(['middleware' => ['permission:register-counterPart']], function () {
-    $this->get('/create', 'CounterPartCreate')->name('counterParts.create');
-    $this->post('/', 'CounterPartStore')->name('counterParts.store');
+Route::group(['middleware' => ['permission:register-counterPart']], function () {
+    Route::get('/create', 'CounterPartCreate')->name('counterParts.create');
+    Route::post('/', 'CounterPartStore')->name('counterParts.store');
 });
 
-$this->group(['middleware' => ['permission:update-counterPart']], function () {
-    $this->get('/{counterPart}/edit', 'CounterPartEdit')->name('counterParts.edit');
-    $this->put('/{counterPart}', 'CounterPartUpdate')->name('counterParts.update');
+Route::group(['middleware' => ['permission:update-counterPart']], function () {
+    Route::get('/{counterPart}/edit', 'CounterPartEdit')->name('counterParts.edit');
+    Route::put('/{counterPart}', 'CounterPartUpdate')->name('counterParts.update');
 });
 
-$this->group(['middleware' => ['permission:register-counterPart']], function () {
-    $this->get('/list', 'CounterPartIndex')->name('counterParts.index');
-    $this->get('/{counterPart}', 'CounterPartShow')->name('counterParts.show');
+Route::group(['middleware' => ['permission:register-counterPart']], function () {
+    Route::get('/list', 'CounterPartIndex')->name('counterParts.index');
+    Route::get('/{counterPart}', 'CounterPartShow')->name('counterParts.show');
 });
 
-$this->group(['middleware' => ['permission:delete-counterPart']], function () {
-    $this->delete('/{counterPart}', 'CounterPartDestroy')->name('counterParts.destroy');
+Route::group(['middleware' => ['permission:delete-counterPart']], function () {
+    Route::delete('/{counterPart}', 'CounterPartDestroy')->name('counterParts.destroy');
 });
