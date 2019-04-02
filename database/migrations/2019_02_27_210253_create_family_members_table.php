f0045e638a74b323bb5cfa248ce1141c269ead97
SUdoWinchester <luizeduardo2607@gmail.com>
2019-04-02 11:14:55 -0300

    Atualizado nome da tabela family_members

---
+++ database/migrations/2019_02_27_210253_create_family_members_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFamilyMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('family_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('birth_date');
+            $table->enum('legal_situation', ['general', 'elderly', 'child', 'disabled-person', 'ex-prisoner']);
+            $table->enum('kinship', [
+                'daughter', 'son', 'cousin', 'sister', 'brother', 'mother', 'father', 'grandmother', 'grandfather', 'uncle', 'aunt',
+                'wife', 'husband', 'grandson', 'granddaughter', 'nephew', 'niece', 'stepfather', 'stepmother', 'stepbrother', 'stepsister'
+            ]);
+            $table->string('work');
+            $table->double('income');
+            $table->integer('assisted_id')->unsigned();
+            $table->foreign('assisted_id')->references('id')->on('assisteds');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('family_members');
+    }
+}
