9451a8a0ca7ea7a2dd26cbfc4904ef86d6da227d
EnViKeyy <pierri.nvk@outlook.com>
2019-04-10 15:15:25 -0300

    Implementado rotas de processo

--- routes/web/process.php
+++ routes/web/process.php
@@ -1,21 +1,25 @@
 <?php
 
-// Assisted routes
-Route::group(['middleware' => ['permission:register-assisted']], function () {
-    Route::get('/create', 'AssistedCreate')->name('assisteds.create');
-    Route::post('/', 'AssistedStore')->name('assisteds.store');
+// Process routes
+Route::group(['middleware' => ['permission:open-process']], function () {
+    Route::get('/create', 'ProcessCreate')->name('processes.create');
+    Route::post('/', 'ProcessStore')->name('processes.store');
 });
 
-Route::group(['middleware' => ['permission:update-assisted']], function () {
-    Route::get('/{assisted}/edit', 'AssistedEdit')->name('assisteds.edit');
-    Route::put('/{assisted}', 'AssistedUpdate')->name('assisteds.update');
+Route::group(['middleware' => ['permission:update-process']], function () {
+    Route::get('/{process}/edit', 'ProcessEdit')->name('processes.edit');
+    Route::put('/{process}', 'ProcessUpdate')->name('processes.update');
+
+    Route::get('/{process}/witnesses', 'ProcessGetWitness')->name('processes.getWitness');
+    Route::put('/{process}/set-witness', 'ProcessSetWitness')->name('processes.setWitness');
+    Route::put('/{process}/unset-witness/{witness}', 'ProcessUnsetWitness')->name('processes.unsetWitness');
 });
 
-Route::group(['middleware' => ['permission:read-assisted']], function () {
-    Route::get('/list', 'AssistedIndex')->name('assisteds.index');
-    Route::get('/{assisted}', 'AssistedShow')->name('assisteds.show');
+Route::group(['middleware' => ['permission:read-process']], function () {
+    Route::get('/list', 'ProcessIndex')->name('processes.index');
+    Route::get('/{process}', 'ProcessShow')->name('processes.show');
 });
 
-Route::group(['middleware' => ['permission:delete-assisted']], function () {
-    Route::delete('/{assisted}', 'AssistedDestroy')->name('assisteds.destroy');
+Route::group(['middleware' => ['role:master']], function () {
+    Route::delete('/{process}', 'ProcessDestroy')->name('processes.destroy');
 });
