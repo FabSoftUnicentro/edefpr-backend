971ddf3bc1e7a50fd1b6afc68827af0d559fd2da
Gianluca Bine <gian_bine@hotmail.com>
2019-04-25 13:25:10 -0300

    Update routes/web/my_files.php
    Co-Authored-By: SUdoWinchester <32457585+SUdoWinchester@users.noreply.github.com>

--- routes/web/my_files.php
+++ routes/web/my_files.php
@@ -5,5 +5,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/list', 'MyFilesIndex')->name('myFiles.index');
     Route::post('/upload', 'MyFilesStore')->name('myFiles.store');
     Route::get('/download/{file}', 'MyFilesDownload')->name('myFiles.download');
-    Route::delete('/{file}', "MyFilesDestroy")->name('myFiles.destroy');
+    Route::delete('/{file}', 'MyFilesDestroy')->name('myFiles.destroy');
 });
