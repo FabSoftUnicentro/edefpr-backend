03ccfc1ab678afbeea91a19774dd65340905cd87
Jean Pierri <pierri.nvk@outlook.com>
2019-05-08 15:35:21 -0300

    Refatoração de código

--- database/factories/FamilyIncomeFactory.php
+++ database/factories/FamilyIncomeFactory.php
@@ -18,13 +18,13 @@ use Illuminate\Support\Facades\DB as DB;
 
 $factory->define(FamilyIncome::class, function (Faker $faker) {
     return [
-        'family_income' => $faker->randomFloat(2),
-        'social_programs' => $faker->randomFloat(2),
-        'social_security_contribution' => $faker->randomFloat(2),
-        'income_tax' => $faker->randomFloat(2),
-        'alimony' => $faker->randomFloat(2),
-        'extraordinary_expenses' => $faker->randomFloat(2),
-        'net_family_income' => $faker->randomFloat(2),
+        'family_income' => $faker->randomFloat(2, 1, 10),
+        'social_programs' => $faker->randomFloat(2, 1, 10),
+        'social_security_contribution' => $faker->randomFloat(2, 1, 10),
+        'income_tax' => $faker->randomFloat(2, 1, 10),
+        'alimony' => $faker->randomFloat(2, 1, 10),
+        'extraordinary_expenses' => $faker->randomFloat(2, 1, 10),
+        'net_family_income' => $faker->randomFloat(2, 1, 10),
         'assisted_id' => DB::table('assisteds')->exists() ? DB::table('assisteds')->inRandomOrder()->first()->id : factory(Assisted::class)->create()
     ];
 });
