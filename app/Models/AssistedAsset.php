4398bcf6ffe16b0857ef8208a7c72046e8f1fc7b
EnViKeyy <pierri.nvk@outlook.com>
2019-05-22 21:18:02 -0300

    Implementado model de bens do assistido

---
+++ app/Models/AssistedAsset.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class AssistedAsset extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'status',
+        'installment_price',
+        'assisted_id'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * Get all of the processes for the assisted.
+     */
+    public function assisted()
+    {
+        return $this->belongsTo(Assisted::class);
+    }
+}
