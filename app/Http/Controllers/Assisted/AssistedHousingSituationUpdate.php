459559d019cf7cf066b69ec5428ea098ad4d7c21
EnViKeyy <pierri.nvk@outlook.com>
2019-05-22 21:14:11 -0300

    Implementado controller para editar situação habitacional do assistido

---
+++ app/Http/Controllers/Assisted/AssistedHousingSituationUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Assisted;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Assisted\UpdateRequest;
+use App\Models\Assisted;
+
+class AssistedHousingSituationUpdate extends Controller
+{
+    /**
+     * @param UpdateRequest $request
+     * @param Assisted $assisted
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function __invoke(UpdateRequest $request, Assisted $assisted)
+    {
+        $assisted->update($request->all());
+
+        try {
+            $assisted->save();
+
+            return redirect()
+                ->route('assisteds.show', $assisted->id)
+                ->with('alert-success', 'Renda familiar atualizada com sucesso!');
+        } catch (\Exception $e) {
+            return redirect()
+                ->back()
+                ->with('alert-danger', 'Falha na atualização da renda familiar!');
+        }
+    }
+}
