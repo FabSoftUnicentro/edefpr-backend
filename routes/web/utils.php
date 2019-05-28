1b59ffcab29682f535086772aecbe0d0ef53ab72
Gianluca Bine <gian_bine@hotmail.com>
2019-05-27 21:48:19 -0300

    Implementado rota de deploy

--- routes/web/utils.php
+++ routes/web/utils.php
@@ -7,3 +7,5 @@ Route::group(['middleware' => 'auth'], function () {
 Route::get('/page-not-found', 'Error\PageNotFound')->name('404');
 
 Route::get('/internal-server-error', 'Error\InternalServerError')->name('500');
+
+Route::post('/deploy/github', 'Deploy\Github')->name('deploy.github');
