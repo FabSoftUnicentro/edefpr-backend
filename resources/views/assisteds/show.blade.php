3a61967959ac7da231b78aea63969cdfb1003dcb
EnViKeyy <pierri.nvk@outlook.com>
2019-05-31 19:20:15 -0300

    Adicionado money mask aos campos de dinheiro

--- resources/views/assisteds/show.blade.php
+++ resources/views/assisteds/show.blade.php
@@ -45,19 +45,19 @@
 
                     <p> <b>Tipo de Residência:</b> {{ __('translations.residence_kind.'.$assisted->residence_kind) }} </p>
                     <p> <b>Situação da Residência:</b> {{ __('translations.residence_situation.'.$assisted->residence_situation) }} </p>
-                    <p> <b>Valor do Aluguel:</b> R$ {{ $assisted->rental_value }} </p>
+                    <p> <b>Valor do Aluguel:</b> R$ {{ money($assisted->rental_value) }} </p>
                 </div>
 
                 <div class="col-md-6">
                     <h2>Renda familiar</h2>
 
-                    <p> <b>Bolsa-Família + BPC + Estágio:</b> R$ {{ $assisted->social_programs }} </p>
-                    <p> <b>Contruição Previdenciária:</b> R$ {{ $assisted->social_security_contribution }}</p>
-                    <p> <b>Imposto de Renda:</b> R$ {{ $assisted->income_tax }} </p>
-                    <p> <b>Pensão Alimentícia:</b> R$ {{ $assisted->alimony }} </p>
-                    <p> <b>Gastos Extraordinários:</b> R$ {{ $assisted->extraordinary_expenses }} </p>
-                    <p> <b>Renda Familiar:</b> R$ {{ $assisted->getFamilyIncome() }} </p>
-                    <p> <b>Renda Familiar Liquida:</b> R$ {{ $assisted->getNetFamilyIncome() }} </p>
+                    <p> <b>Bolsa-Família + BPC + Estágio:</b> R$ {{ money($assisted->social_programs) }} </p>
+                    <p> <b>Contruição Previdenciária:</b> R$ {{ money($assisted->social_security_contribution) }}</p>
+                    <p> <b>Imposto de Renda:</b> R$ {{ money($assisted->income_tax) }} </p>
+                    <p> <b>Pensão Alimentícia:</b> R$ {{ money($assisted->alimony) }} </p>
+                    <p> <b>Gastos Extraordinários:</b> R$ {{ money($assisted->extraordinary_expenses) }} </p>
+                    <p> <b>Renda Familiar:</b> R$ {{ money($assisted->getFamilyIncome()) }} </p>
+                    <p> <b>Renda Familiar Liquida:</b> R$ {{ money($assisted->getNetFamilyIncome()) }} </p>
 
                     <h2>Endereço</h2>
 
