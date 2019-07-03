849f22172e79c27bb510d5226b71c964c2114887
SUdoWinchester <luizeduardo2607@gmail.com>
2019-07-03 19:59:48 -0300

    Implementado controller de deletar petição

---
+++ app/Http/Controllers/Process/ProcessUnsetPetition.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Process;
+
+use App\Http\Controllers\Controller;
+use App\Models\Process;
+use Illuminate\Http\JsonResponse;
+use Spatie\MediaLibrary\MediaStream;
+
+class ProcessUnsetPetition extends Controller
+{
+    /**
+     * @param Processs $process
+     * @param $files
+     * @return JsonResponse
+     */
+    public function __invoke(Process $process, $files)
+    {
+        $fileIds = explode(',', $files);
+        $fileIdsTotal = count($fileIds);
+
+        if ($fileIdsTotal >= 1) {
+            if ($fileIdsTotal === 1) {
+                $file = $process->getMedia('petition')->find($fileIds)->first();
+
+                $file->delete();
+
+                return JsonResponse::create([
+                    'file' => $file->toJson()
+                ], 200);
+            } else {
+                $files = $process->getMedia('petition')->find($fileIds);
+
+                foreach ($files as $file) {
+                    $file->delete();
+                }
+
+                return JsonResponse::create([
+                    'files' => $files->toArray()
+                ], 200);
+            }
+        }
+
+        return response()->json([
+            'message' => 'File not found'
+        ]);
+    }
+}
