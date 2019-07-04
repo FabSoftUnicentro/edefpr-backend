e65d5c05f4609db67df704d402fa21b003d9e8a1
Luiz Eduardo Chicouski da Cruz <32457585+SUdoWinchester@users.noreply.github.com>
2019-07-04 08:41:13 -0300

    Update routes/web/logActivity.php
    Co-Authored-By: Gianluca Bine <gian_bine@hotmail.com>

--- routes/web/logActivity.php
+++ routes/web/logActivity.php
@@ -1,6 +1,6 @@
 <?php
 
-//Log Activity routes
+// Log Activity routes
 Route::group(['middleware' => ['permission:register-activities']], function () {
     Route::get('/list', 'LogActivityIndex')->name('logActivity.index');
     Route::get('/filter', 'LogActivityFilter')->name('logActivity.filter');
